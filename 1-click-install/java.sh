fe7e6197fd51270c15f799904c5e5233fe58ceda
Darshan S Mahendrakar <darshansm22@gmail.com>
2015-04-24 00:26:15 -0700

    Update java.sh

--- 1-click-install/java.sh
+++ 1-click-install/java.sh
@@ -23,14 +23,8 @@ EOF
 cat > /etc/hosts <<\EOF
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-10.2.1.58   as1.novalocal  as1  #172.16.2.107   #Medium
-10.2.1.56   as2.novalocal  as2  #172.16.2.108   #Medium
-10.2.1.57   as3.novalocal  as3  #172.16.2.109   #Medium
-10.2.1.65   as4.novalocal  as4  #172.16.3.11    #Large
-10.2.1.162  as5.novalocal  as5  #172.16.3.13    #Large
-10.2.1.161  as6.novalocal  as6  #172.16.3.12    #Large
-10.2.1.83   as7.novalocal  as7  #172.16.0.77    #Large
-10.2.1.33   as8.novalocal  as8  #172.16.2.242   #Large
+#Mention the hostname 
+#PrivateID FQDN dn 
 EOF
 
 ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
