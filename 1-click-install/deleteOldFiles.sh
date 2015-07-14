894a8e72f4b465082c9cfd5ba07a0eab0a2cbd2d
Darshan S Mahendrakar <darshansm22@gmail.com>
2015-07-13 20:19:50 -0700

    Update deleteOldFiles.sh

--- 1-click-install/deleteOldFiles.sh
+++ 1-click-install/deleteOldFiles.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
 #Author: Darshan S Mahendrakar
-#Date: 07/13/15
+#Date:07/13/15
 
-path=/root/darshan/*
-file_type =  "*.txt, *.wsp"
+mkdir -p /tmp/1837 && cd /tmp/1837
 
-for i in $file_type
-        do
-        find $path -name "$i" -mtime +90 -type f -delete
-        done
+path=/root/darshan/
+file_types="*.txt *.wsp"
+
+#echo $file_types
+for i in $file_types
+do 
+	find $path -name "$i" -mtime +90 -type f -delete
+done
+
+rm -rf /tmp/1837
