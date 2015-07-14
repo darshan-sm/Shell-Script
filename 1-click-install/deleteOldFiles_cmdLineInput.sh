418b4496a941af89b083ea2f2c349f9f276df7f1
Avinash Reddy Desireddy <avinashreddy1090@outlook.com>
2015-07-14 08:08:30 -0400

    Update deleteOldFiles_cmdLineInput.sh

--- 1-click-install/deleteOldFiles_cmdLineInput.sh
+++ 1-click-install/deleteOldFiles_cmdLineInput.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
-#Author: Darshan S Mahendrakar
-#Date: 07/13/15
+# Author: Darshan S Mahendrakar
+# Date Created: 07/13/15
 
-#mkdir /tmp/deleteOld && cd /tmp/deleteOld
+# Last Updated: 07/14/15
 
+## Initializing variables
+ROOT_PATH=$1
+FILE_TYPE=$2
+NO_OF_DAYS=$3
 
-#path=/root/darshan/*
-#file_type =  "*.txt, *.wsp"
+TMP_DELETE_FILE=/tmp/toDelete.tmp
+DELETE_LOG=delete.log
 
-echo "Enter The Path"
-  read path
-echo "Enter the file type"
-  read file_type
-echo "Enter how many days old files"
-  read days
-echo "Starting to delete files"
-find $path -name "$file_type" -mtime "+$days" -type f -exec rm -r {} \;
-echo "files deleted"
-#rm -rf /tmp/deleteOld/
+# function to validate the given arguments
+function validateArguments() {
+	# checking argument ROOT_PATH
+	if [ -z $ROOT_PATH ]
+		then
+			echo "ROOT path not specified"
+			exit 1
+	fi
+	
+	# checking argument FILE_TYPE
+	if [ -z $FILE_TYPE ]
+		then
+			echo "File type not specified"
+			exit 1
+	fi
+	
+	# checking argument NO_OF_DAYS
+	if [ -z $NO_OF_DAYS ]
+		then
+			echo "ROOT path not specified"
+			exit 1
+	fi
+}
+
+# function to find files older than X days
+function findFiles() {
+	find $ROOT_PATH -type f -name $FILE_TYPE -mtime +$NO_OF_DAYS > $TMP_DELETE_FILE
+	#commong log file
+	cat $TMP_DELETE_FILE >> $DELETE_LOG
+}
+
+# function that deletes files that are recorded in TMP_DELETE_FILE
+function deleteFiles() {
+	for file in $(cat $TMP_DELETE_FILE)
+	do
+		echo "$file"
+		# rm -rf $file
+	done
+}
+
+# main_program
+validateArguments
+findFiles
+deleteFiles
+
