97651d8ffe14c289a02849e8414165eeffb05710
root <root@cdh00.novalocal>
2015-07-24 17:40:43 -0400

    testing

---
+++ testing/deleteOld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Author: Darshan S Mahendrakar
+#Date: 07/13/15
+
+#mkdir /tmp/deleteOld && cd /tmp/deleteOld
+
+
+#path=/root/darshan/*
+#file_type =  "*.txt, *.wsp"
+
+#echo "Enter The Path"
+  path=$path
+#echo "Enter the file type"
+  read file_type
+#echo "Enter how many days old files"
+  read days
+echo "Starting to delete files"
+find $path -name "$file_type" -mtime "+$days" -type f -exec rm -r {} \; 
+echo "files deleted"
+#rm -rf /tmp/deleteOld/
+	
